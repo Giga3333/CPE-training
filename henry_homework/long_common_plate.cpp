4b809fb84860f8d250178d87661eac24e1be4550
Giga <giga@GigadeMacBook-Air.local>
2022-12-02 22:50:27 +0800

    giga is stupid

---
+++ henry_homework/long_common_plate.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    // int count = 0, in_count = 0;
+    // for (int i = 0; i < s.size(); i++)
+    //{
+    //
+    //    if (s[i] == s[i + 1])
+    //    {
+    //        in_count++;
+    //        // if (in_count > count)
+    //        //     count = in_count;
+    //        count = max(count, in_count);
+    //    }
+    //    else
+    //        in_count = 0;
+    //}
+    //
+    // cout << count + 1 << "\n";
+
+    int len = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == s[i - len])
+            len++;
+    }
+    cout << len << "\n";
+};
